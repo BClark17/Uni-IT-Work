42b6cb073fc35d4e3111aa1cc90bed8cd0b9dd77
jc463696 <bailey.clark@my.jcu.edu.au>
2018-03-13 09:18:30 +1000

    9:17 Changes

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -4,11 +4,11 @@ Broken program to determine score status
 """
 
 score = float(input("Enter your score:"))
-if float(score) < 50 and float(score) >= 0:
+if 0 <= float(score) < 50:
     print("Bad Score")
-elif float(score) < 90 and float(score) >= 50:
+elif 50 <= float(score) < 90:
     print("Passable Score")
-elif float(score) < 100 and float(score) >= 90:
+elif 90 <= float(score) < 100:
     print("Excellent")
 else:
     print("Please input a score value from 0 to 100.")
